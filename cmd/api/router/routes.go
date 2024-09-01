3582aa71c7c8f1cbcc2028796f91414ffd123281
Andre Posman <andre.posman@mercadolivre.com>
2024-09-01 00:36:21 -0300

    feat: adding more initial routes

--- cmd/api/router/routes.go
+++ cmd/api/router/routes.go
@@ -12,6 +12,9 @@ func initRoutes(router *gin.Engine) {
 	{
 		v1.GET("/ping", pingHandler)
 		v1.GET("/opening", openingHandler)
+		v1.POST("/opening", createOpeningHandler)
+		v1.PUT("/opening", updateOpeningHandler)
+		v1.DELETE("/opening", deleteOpeningHandler)
 	}
 }
 
@@ -24,5 +27,23 @@ func pingHandler(ctx *gin.Context) {
 func openingHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"message": "GET Opening",
+	})
+}
+
+func createOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "POST Opening",
+	})
+}
+
+func updateOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "PUT Opening",
+	})
+}
+
+func deleteOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "DELETE Opening",
 	})
 }
